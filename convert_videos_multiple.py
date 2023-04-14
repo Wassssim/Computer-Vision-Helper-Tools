3f2eb421744aca3656e6469e4393ea7a20486c7f
Wassssim <chadhabourbia758@gmail.com>
2023-04-14 17:47:18 +0100

    added convert videos multiple

---
+++ convert_videos_multiple.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import shutil
+from subprocess import call
+from os import listdir
+from os.path import isdir, isfile, join
+
+def listVideos(videoDir):
+    return [f for f in listdir(videoDir) if isfile(join(videoDir, f)) and ".mp4" in f]
+
+
+def moveVideos(srcDir, destDir):
+    videos = listVideos(srcDir)
+    for videoFilename in videos:
+        videoAbspath = join(srcDir, videoFilename)
+        shutil.move(videoAbspath, destDir)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("[ERROR] Please provide an input path")
+        sys.exit()
+
+    input_path = (sys.argv[1]).replace("\\", "/")
+
+    args={
+        "input": input_path,
+    }
+
+    print("Videos Directories: ", args["input"])
+
+    videoDirs = [d for d in listdir(args["input"]) if isdir(join(args["input"], d))]
+
+    for videoDir in videoDirs:
+        videoDirAbspath = join(args["input"], videoDir)
+        rawDirAbspath = join(videoDirAbspath, "raw")
+
+        # Check whether the specified path exists or not
+        isExist = os.path.exists(rawDirAbspath)
+        if not isExist:
+            # Create a new directory because it does not exist 
+            os.makedirs(rawDirAbspath)
+        
+        print("The output directory is created")
+        print("==> Converting:", join(args["input"], videoDir))
+        returnCode = call(f'python3 convert_videos.py "{join(args["input"], videoDir)}"')
+        if (returnCode == 0):
+            moveVideos(videoDirAbspath, rawDirAbspath)
+        else:
+            stop = False
+            while (True):
+                answer = input("ERROR in conversion command, continue? [y/n]")
+                if (answer.upper() == "Y"):
+                    break 
+                elif (answer.upper() == "N"):
+                    stop = True
+                    break
+            if (stop): 
+                break
+
+
+
+if (__name__ == '__main__'):
+    main()
--- download_excels.py
+++ download_excels.py
@@ -5,19 +5,6 @@ import requests
 from os import listdir
 from os.path import join, isdir, exists
 
-input_abspath = "/media/rtxdeepstream/INTENSO1/LOT3/RGC OCTOBRE 2022-SEMAINE 1"
-output_abs_path= "/media/rtxdeepstream/INTENSO1/LOT3/RGC OCTOBRE 2022-SEMAINE 1/Excels"
-api_base_url = "http://ns3089849.ip-54-36-61.eu:3002/api"
-
-sites = [ d for d in listdir(input_abspath) if isdir(join(input_abspath, d))]
-
-# login
-userData = {
-    "username": "wassim",
-    "password": "12345"
-}
-res = requests.post(f"{api_base_url}/user/login", headers={"Accept":"application/json"}, json=userData)
-token = f"Bearer {res.json()['token']}"
 
 def parse_date(dateStr):
     date = datetime.datetime.strptime(dateStr, '%Y-%m-%d %H:%M:%S')
@@ -25,34 +12,58 @@ def parse_date(dateStr):
     iso_date = utc_date.strftime('%Y-%m-%dT%H:%M:%S')
     return iso_date
 
+def prompt_credentials():
+    username = input("Username: ")
+    password = input("Password: ")
+    return {
+        "username": username,
+        "password": password
+    }
 
-for site_dir in sites:
-    site_dir_abspath = join(input_abspath, site_dir)
-    
-    site_id = -1
-    config_file_abspath = join(site_dir_abspath, "config.json")
-    if not (exists(config_file_abspath)):
-        print("Skipping", site_dir_abspath)
-        continue
-    
-    with open(config_file_abspath, "r") as cfg:
-        site_id = json.load(cfg)["site_id"]
-        res = requests.get(f"{api_base_url}/sites/{site_id}/planning", headers={"Accept":"application/json", "Authorization": token})
-        plannings = res.json()["plannings"]
+def get_token(user_credentials):
+    res = requests.post(f"{api_base_url}/user/login", headers={"Accept":"application/json"}, json=user_credentials)
+    res.raise_for_status()
+    return f"Bearer {res.json()['token']}"
 
-        outdir = join(output_abs_path, site_dir)
-        if not (os.path.exists(outdir)):
-            os.mkdir(outdir)
+
+def main():
+    input_abspath = "/media/rtxdeepstream/INTENSO1/LOT3/RGC OCTOBRE 2022-SEMAINE 1"
+    output_abs_path= "/media/rtxdeepstream/INTENSO1/LOT3/RGC OCTOBRE 2022-SEMAINE 1/Excels"
+    api_base_url = "http://ns3089849.ip-54-36-61.eu:3002/api"
+
+    sites = [ d for d in listdir(input_abspath) if isdir(join(input_abspath, d))]
+
+    # login
+    user_credentials = prompt_credentials()
+    token = get_token(user_credentials)
+
+    for site_dir in sites:
+        site_dir_abspath = join(input_abspath, site_dir)
         
-        for idx, planning in enumerate(plannings):
-            begin_date = parse_date(planning["start_date"])
-            end_date = parse_date(planning["end_date"])
+        site_id = -1
+        config_file_abspath = join(site_dir_abspath, "config.json")
+        if not (exists(config_file_abspath)):
+            print("Skipping", site_dir_abspath)
+            continue
+        
+        with open(config_file_abspath, "r") as cfg:
+            site_id = json.load(cfg)["site_id"]
+            res = requests.get(f"{api_base_url}/sites/{site_id}/planning", headers={"Accept":"application/json", "Authorization": token})
+            plannings = res.json()["plannings"]
 
-            res = requests.get(f"{api_base_url}/sites/{site_id}/census-report/?beginDate={begin_date}&endDate={end_date}", headers={"Accept":"application/json", "Authorization": token})
+            outdir = join(output_abs_path, site_dir)
+            if not (os.path.exists(outdir)):
+                os.mkdir(outdir)
             
-            excel_filename = f'{site_dir}.xlsx' if idx == 0 else f'{site_dir} - {idx}.xlsx'
+            for idx, planning in enumerate(plannings):
+                begin_date = parse_date(planning["start_date"])
+                end_date = parse_date(planning["end_date"])
 
-            with open(join(outdir, excel_filename), 'wb') as f:
-                # Write the binary content of the response to the file
-                f.write(res.content)
-            
+                res = requests.get(f"{api_base_url}/sites/{site_id}/census-report/?beginDate={begin_date}&endDate={end_date}", headers={"Accept":"application/json", "Authorization": token})
+                
+                excel_filename = f'{site_dir}.xlsx' if idx == 0 else f'{site_dir} - {idx}.xlsx'
+
+                with open(join(outdir, excel_filename), 'wb') as f:
+                    # Write the binary content of the response to the file
+                    f.write(res.content)
+                
